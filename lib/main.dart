d92240df97e30e163f90ec0e436b6650206434d1
Ibuu31 <ibrahimbode41@gmail.com>
2023-09-23 20:16:52 +0530

    Changes in the UI by removing unnecessary code and reusing the Widget at two places.

--- lib/main.dart
+++ lib/main.dart
@@ -37,71 +37,78 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Consumer<Counter>(
-          builder: (BuildContext context, Counter counter, Widget? child) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'You have pushed the button this many times:',
-                  ),
-                  Text(
-                    // taking the value of _counter variable from the Counter page.
-                    '${counter._counter}',
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                          height: 65,
-                          width: 65,
-                          decoration: BoxDecoration(
-                              color: Colors.cyanAccent,
-                              borderRadius: BorderRadius.circular(100)),
-                          child: IconButton(
-                              onPressed: () {
-                                counter.increaseCount();
-                              },
-                              icon: const Icon(Icons.add))),
-                      Container(
-                          height: 65,
-                          width: 65,
-                          decoration: BoxDecoration(
-                              color: Colors.deepOrangeAccent,
-                              borderRadius: BorderRadius.circular(100)),
-                          child: IconButton(
-                              onPressed: () {
-                                counter.decreaseCount();
-                              },
-                              icon: const Icon(Icons.remove))),
-                    ],
-                  )
-                ],
-              ),
-            );
-          },
-        ),
-        floatingActionButton: Consumer<Counter>(
-          builder: (BuildContext context, Counter value, Widget? child) {
-            return FloatingActionButton(
-              onPressed: () {
-                // calling the increaseCount method from Counter page.
-                value.increaseCount();
-              },
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
-            );
-          },
-        ));
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      // Consumer
+      body: Consumer<Counter>(
+        builder: (BuildContext context, Counter counter, Widget? child) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  // taking the value of _counter variable from the Counter class.
+                  '${counter._counter}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    PlusOrMinusButton(
+                      color: Colors.cyanAccent,
+                      iconButton: IconButton(
+                          onPressed: () {
+                            // calling the increaseCount method from the Counter class to add the counter by 1.
+                            counter.increaseCount();
+                          },
+                          icon: const Icon(Icons.add)),
+                    ),
+                    PlusOrMinusButton(
+                        iconButton: IconButton(
+                            onPressed: () {
+                              // calling the decreaseCount method from the Counter class to subtract the counter by 1.
+                              counter.decreaseCount();
+                            },
+                            icon: const Icon(Icons.remove)),
+                        color: Colors.deepOrangeAccent)
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+//here we have created a new Widget for both the buttons that we are interacting on the UI screen from which we are increasing or decreasing the counter value.
+class PlusOrMinusButton extends StatelessWidget {
+  const PlusOrMinusButton({
+    super.key,
+    required this.iconButton,
+    required this.color,
+  });
+
+  final IconButton iconButton;
+  final MaterialAccentColor color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 65,
+        width: 65,
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.circular(100)),
+        child: iconButton);
   }
 }
 
@@ -114,6 +121,7 @@ class Counter extends ChangeNotifier {
     notifyListeners();
   }
 
+// this method will decrement the counter number by 1 on pressing the minus button on the screen
   void decreaseCount() {
     _counter--;
     notifyListeners();
